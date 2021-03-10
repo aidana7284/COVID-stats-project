8d825c840a21559506f71d8c8ab7924eaf732117
Aidan Andres <aidana7284@hstat.org>
2021-03-10 17:07:33 +0000

    update repo

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+   var userDate = prompt("What date would you like information on? Put in xx/xx/xxxx format.(From 02/29/2020-12/03/2020)")
 $.ajax({
-    url: 'https://data.cityofnewyork.us/Health/COVID-19-Daily-Counts-of-Cases-Hospitalizations-an/rc75-m7u3',
+    url: 'https://data.cityofnewyork.us/resource/rc75-m7u3.json',
     dataType: 'json',
     success: function(data) {
         console.log(data);
-        var userDate = prompt("What date would you like information on? Put in xx/xx/xxxx format.(From 02/29/2020-12/03/2020)")
         var data = "05/04/2020"
         for (var i=0; i < data.length; i++){
             if (data[i].date == ("  " + userDate)){
