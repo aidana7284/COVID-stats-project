27bbb101b1d0ab73b14ae30e9e6706997c401429
Matthew Regan <matthewr6745@hstat.org>
2021-03-10 16:54:17 +0000

    update repo

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 $.ajax({
-    url: 'https://api.publicapis.org/entries',
+    url: 'https://data.cityofnewyork.us/Health/COVID-19-Daily-Counts-of-Cases-Hospitalizations-an/rc75-m7u3',
     dataType: 'json',
     success: function(data) {
         console.log(data);
-    }
+        var userDate = prompt("What date would you like information on? Put in xx/xx/xxxx format.(From 02/29/2020-12/03/2020)")
+        var data = "05/04/2020"
+        for (var i=0; i < data.length; i++){
+            if (data[i].date == ("  " + userDate)){
+                var count = (data[i]["_" + userDate])
+                document.write("There were " + count + " case(s) on" + userDate + ".")
+            };
+        };
+    };
 });
