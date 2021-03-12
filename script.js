34559d95b7a055fd8c297b965f38ae5c015f4a60
Aidan Andres <aidana7284@hstat.org>
2021-03-12 01:08:07 +0000

    update repo

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-   //var userDate = prompt("What date would you like information on? Put in xx/xx/xxxx format.(From 02/29/2020-12/03/2020)")
-   var userDate = ("2020-02-29T00:00:00.000")
+   var userDate = prompt("What date would you like information on? Put in (year-month-date) xxxx-xx-xx format.(From 2020-02-29 to 2020-12-03)")
+   //var userDate = ("2020-02-29T00:00:00.000")
 $.ajax({
     url: 'https://data.cityofnewyork.us/resource/rc75-m7u3.json',
     dataType: 'json',
     success: function(data) {
         console.log(data);
         for (var i = 0; i < data.length; i++) {
-            if (data[i].date_of_interest == (userDate)){
+            if (data[i].date_of_interest == (userDate + "T00:00:00.000")){
                //var count = (data[i]["_" + userDate])
                 //var caseCount = (data[i]["date_of_interest" + "_" + userDate + "T00:00:00.000" + "_CASE_COUNT"])
                 //document.write("There were " + count + " case(s) on" + userDate + ".")
-                document.write("hello");
+                var count = (data[i].case_count);
+                var death = (data[i].death_count);
+                document.write("There were " + count + " case(s) and " + death + " death(s)" + " on " + userDate);
             }
         }
     }
